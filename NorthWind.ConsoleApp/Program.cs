21b75c5c52a421e8afb3f9277bbd7248a03dce52
Akaren2022 <karenaitana21@gmail.com>
2024-10-15 23:01:02 -0500

    Console App para consumir la interfaz

---
+++ NorthWind.ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using NorthWind.ConsoleApp.Services;
+using NorthWind.Entities.Interfaces;
+
+IUserActionWriter writer = default;
+
+AppLogger logger = new AppLogger(writer);
+logger.WriteLog("Application Started");
---
+++ NorthWind.ConsoleApp/Services/AppLogger.cs
@@ -0,0 +1,19 @@
+﻿using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.ConsoleApp.Services
+{
+	internal class AppLogger(IUserActionWriter writter)
+	{
+		public void WriteLog(string messagge)
+		{ 
+			UserAction Log = new UserAction("System", messagge);
+			writter.Write(Log);
+		}
+	}
+}
